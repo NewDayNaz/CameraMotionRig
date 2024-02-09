7e00c1d301a139f8407bda7a3a4fdf9ce913089f
NewDayNaz <mgraham@newdaynaz.org>
2024-02-09 14:50:36 -0600

    Added MIDI-Cmd-Server

---
+++ MIDI-Cmd-Server/camera_midi.js
@@ -0,0 +1,45 @@
+var easymidi = require('easymidi');
+const request = require('request');
+
+async function init() {
+    var url = "http://10.10.10.112:8080/";
+    var input = new easymidi.Input('NewDay-Stream');
+    input.on('noteon', async function (msg) {
+    // do something with msg
+    if (msg.channel == 1) {
+        if (msg.velocity > 0) {
+            if (msg.note == 1) {
+                await sleep(400);
+                request(url + '?cmd=t', { json: true }, (err, res, body) => {});
+            }
+            if (msg.note == 2) {
+                await sleep(400);
+                request(url + '?cmd=t2', { json: true }, (err, res, body) => {});
+            }
+            if (msg.note == 4) {
+                await sleep(400);
+                request(url + '?cmd=t3', { json: true }, (err, res, body) => {});
+            }
+            if (msg.note == 3) {
+                await sleep(400);
+                request(url + '?cmd=t4', { json: true }, (err, res, body) => {});
+            }
+            console.log(msg);
+        }
+    }
+    });
+
+    // CAM POS PLAN
+    // Pos 1 = Blake
+    // Pos 2 = Singers
+    // Pos 3 = Middle
+    // Pos 4 = James
+};
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+init();
